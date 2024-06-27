21f424a631057ebd91cca9f79a3f93cbc0d90a7e
saffronrust <saffronrust@gmail.com>
2024-06-27 23:58:06 +0800

    added documentation for loadEvents.js

--- src/handlers/loadEvents.js
+++ src/handlers/loadEvents.js
@@ -1,6 +1,10 @@
 const clientEvent = (event) => require(`../events/client/${event}`)
 const guildEvent = (event) => require(`../events/guild/${event}`)
 
+/**
+ * This function loads the events in the client and guild folders
+ * @param {client} client The discord client
+ */
 function loadEvents(client) {
 	client.on("ready", () => clientEvent("ready")(client))
 	client.on("interactionCreate",(m) => guildEvent("slashCommands")(m, client))

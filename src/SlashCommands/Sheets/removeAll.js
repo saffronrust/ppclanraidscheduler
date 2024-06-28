06cd0e837ab5574eca58af03505ff26780103ba4
Zheng Jie <zhengjie@istaging.com>
2024-06-28 11:39:59 +0800

    feat: Add removeAll command to clear the entire sheet

--- src/SlashCommands/Sheets/removeAll.js
+++ src/SlashCommands/Sheets/removeAll.js
@@ -1,1 +1,33 @@
-// clears the entire sheet, except for headers (aka first row)
+const { successEmbedFunc } = require("../../utility/embeds/successEmbed");
+
+module.exports = {
+	name: "removeall",
+	description: "Clears the entire sheet",
+	userPerms: ["ADMINISTRATOR"],
+	options: null,
+	run: async(client, interaction, args) => {
+
+		await client.googleSheets.batchUpdate({
+			auth: client.auth,
+			spreadsheetId: client.sheetId,
+			resource: {
+					"requests": [
+					{
+						"deleteDimension": {
+							"range": {
+								"sheetId": 0,
+								"dimension": "ROWS",
+								"startIndex": 1,
+								"endIndex": 99,
+							},
+						}
+					}
+				]
+			}
+		}).catch(console.error)
+
+		console.log("Sheet cleaned")
+		const successEmbed = successEmbedFunc(`Sheet has been cleaned`)
+		await interaction.reply({ embeds: [successEmbed] })
+	}
+}
